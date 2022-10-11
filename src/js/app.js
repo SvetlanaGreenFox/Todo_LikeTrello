6d240426ca38e51c7b48f510d1fe6f8f1ab6c120
Светлана <svetlanagreenfox@gmail.com>
2022-10-11 21:06:49 +0300

    Added dragDrop

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,91 @@
 const textareaElements = document.querySelectorAll('.form-control');
 
-Array.from(textareaElements).forEach((textArea) => textArea.addEventListener('keyup', (event) => {
-  textArea.style.height = 'auto';
-  const scHeight = event.target.scrollHeight;
-  textArea.style.height = `${scHeight}px`;
-}));
+textareaElements.forEach((elem) =>
+  elem.addEventListener('keyup', (event) => {
+    elem.style.height = 'auto';
+    let scHeight = event.target.scrollHeight;
+    elem.style.height = `${scHeight}px`;
+  })
+);
 
-const addingCards = document.querySelectorAll('.adding__cards');
-Array.from(addingCards).forEach((elem) => {
+const highlightCard = function () {
+  this.classList.toggle('highlight');
+  const parentEl = this.parentElement;
+  const closeBtn = this.querySelector('.delete-button');
+  closeBtn.classList.toggle('hidden');
+  closeBtn.addEventListener('click', () => {
+    this.remove();
+    const taskList = parentEl.querySelectorAll('.task');
+    if (taskList.length < 1) {
+      const previoslyTask = parentEl.querySelector('.pre-task');
+      previoslyTask.classList.remove('hidden');
+    }
+  });
+};
+
+const dragDrop = function () {
+  const draggedEl = this;
+  const ghostEl = this.cloneNode(true);
+
+  this.addEventListener('mousedown', () => {
+    ghostEl.classList.add('dragged');
+    document.body.append(ghostEl);
+  });
+
+  this.addEventListener('mousemove', (event) => {
+    event.preventDefault();
+    if (!draggedEl) return;
+    ghostEl.style.left = event.pageX - ghostEl.offsetWidth / 2 + 'px';
+    ghostEl.style.top = event.pageY - ghostEl.offsetHeight / 2 + 'px';
+  });
+
+  this.addEventListener('mouseup', (event) => {
+    if (!draggedEl) return;
+    let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+  });
+};
+
+const addCard = function (event) {
+  const targetParent = this.closest('.card-group');
+  const targetForm = targetParent.querySelector('form');
+
+  const targetTextarea = targetForm.querySelector('.form-control');
+
+  event.preventDefault();
+  if (targetTextarea.value.length !== 0) {
+    const taskGroup = targetParent.querySelector('.task-group');
+    const newTask = document.createElement('li');
+    newTask.classList.add('task');
+    newTask.innerHTML = `<div class="task-description">
+        <span class="task-title">${targetTextarea.value}</span>
+        <span class="material-symbols-outlined delete-button hidden">
+          disabled_by_default
+        </span>
+      </div>
+      <span class="material-symbols-outlined task-menu">
+      density_medium
+    </span>`;
+
+    taskGroup.appendChild(newTask);
+
+    targetForm.classList.add('hidden');
+    targetParent.querySelector('.adding__cards').classList.remove('hidden');
+    newTask.addEventListener('click', highlightCard);
+    newTask.addEventListener('mousedown', dragDrop);
+  }
+  const taskList = targetParent.querySelectorAll('.task');
+  const previoslyTask = targetParent.querySelector('.pre-task');
+
+  if (taskList.length > 0) {
+    previoslyTask.classList.add('hidden');
+  }
+
+  targetTextarea.value = '';
+};
+
+const adding__cards = document.querySelectorAll('.adding__cards');
+
+adding__cards.forEach((elem) => {
   elem.addEventListener('click', () => {
     const targetParent = elem.parentElement;
     const targetForm = targetParent.querySelector('form');
@@ -19,51 +97,8 @@ Array.from(addingCards).forEach((elem) => {
 
     const targetButtonAdd = targetForm.querySelector('.add__task');
     const targetCloseButton = targetForm.querySelector('.close-button');
-    const targetTextarea = targetForm.querySelector('.form-control');
 
-    targetButtonAdd.addEventListener('click', (event) => {
-      event.preventDefault();
-      if (targetTextarea.value.length !== 0) {
-        const taskGroup = targetParent.querySelector('.task-group');
-        const newTask = document.createElement('li');
-        newTask.classList.add('task');
-        newTask.innerHTML = `<div class="task-description">
-        <span class="task-title">${targetTextarea.value}</span>
-        <span class="material-symbols-outlined delete-button hidden">
-          disabled_by_default
-        </span>
-      </div>
-      <span class="material-symbols-outlined task-menu">
-      density_medium
-    </span>`;
-
-        taskGroup.appendChild(newTask);
-        targetForm.classList.add('hidden');
-        elem.classList.remove('hidden');
-      }
-
-      const taskList = targetParent.querySelectorAll('.task');
-      const previoslyTask = targetParent.querySelector('.pre-task');
-
-      if (taskList.length >= 1) {
-        previoslyTask.classList.add('hidden');
-        Array.from(taskList).forEach((elem) => elem.addEventListener('click', () => {
-          if (elem.classList.contains('pre-task')) return;
-          elem.classList.toggle('highlight');
-          const closeBtn = elem.querySelector('.delete-button');
-          closeBtn.classList.toggle('hidden');
-          closeBtn.addEventListener('click', () => {
-            elem.remove();
-            const taskList = targetParent.querySelectorAll('.task');
-            if (taskList.length < 1) {
-              previoslyTask.classList.remove('hidden');
-            }
-          });
-        }));
-      }
-
-      targetTextarea.value = '';
-    });
+    targetButtonAdd.addEventListener('click', addCard);
 
     targetCloseButton.addEventListener('click', () => {
       targetForm.classList.add('hidden');
