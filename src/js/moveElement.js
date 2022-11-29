94ba635b0ce08180d938714a7fe1fffab52676af
Светлана <svetlanagreenfox@gmail.com>
2022-11-29 22:10:45 +0300

    Corrected task-list__item in moveElement

--- src/js/moveElement.js
+++ src/js/moveElement.js
@@ -2,7 +2,7 @@ export default function moveElement(event, element) {
   const closest = document.elementFromPoint(event.clientX, event.clientY);
   const { top } = closest.getBoundingClientRect();
 
-  if (closest.classList.contains('task-list-item')) {
+  if (closest.classList.contains('task-list__item')) {
     if (event.pageY > window.scrollY + top + closest.offsetHeight / 2) {
       closest
         .closest('.list__task-list')
@@ -10,7 +10,7 @@ export default function moveElement(event, element) {
     } else {
       closest.closest('.list__task-list').insertBefore(element, closest);
     }
-  } else if (closest.classList.contains('list__task-list') && !closest.querySelector('.task-list-item')) {
+  } else if (closest.classList.contains('list__task-list') && !closest.querySelector('.task-list__item')) {
     closest.append(element);
   }
 }
